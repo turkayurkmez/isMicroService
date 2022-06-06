c871cec3421f19b226518aff7f244a498945536c
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-06-06 15:10:28 +0300

    Add docker tools command to the root

---
+++ MicroServiceSolution/src/Services/Catalog.API/Data/CatalogDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Catalog.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.API.Data
+{
+    public class CatalogDbContext : DbContext
+    {
+        public CatalogDbContext(DbContextOptions<CatalogDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+      
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Category>().HasData(new Category  {  Id = 1, Name = "Giyim"},
+                                               new Category { Id=2, Name="Elektronik"} 
+                );
+
+            var products = new List<Product>
+            {
+                new Product{ Id=1, Name="Çamaşır Makinesi", Price=100, CategoryId=2, ImageUrl="https://picsum.photos/seed/picsum/200/300"},
+                new Product{ Id=2, Name="Tişört", Price=100, CategoryId=1, ImageUrl="https://picsum.photos/seed/picsum/200/300"},
+                new Product{ Id=3, Name="Şapka", Price=100, CategoryId=2, ImageUrl="https://picsum.photos/seed/picsum/200/300"},
+                
+
+
+            };
+
+        }
+
+        
+    }
+   
+}
--- MicroServiceSolution/src/Services/Catalog.API/Program.cs
+++ MicroServiceSolution/src/Services/Catalog.API/Program.cs
@@ -1,3 +1,6 @@
+using Catalog.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<CatalogDbContext>(config => config.UseSqlServer());
+
 
 var app = builder.Build();
 
